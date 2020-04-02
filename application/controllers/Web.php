1c727d196b1a3185a1b3fdf5bb665f8297a100a2
Jual Koding <jualkoding@gmail.com>
2020-04-02 09:28:39 +0700

    kembalikn untuk bisa di pakai DEMO

--- application/controllers/Web.php
+++ application/controllers/Web.php
@@ -16,49 +16,51 @@ class Web extends CI_Controller {
 
 	public function cek_pin($pin)
 	{
-		$db_lsima = $this->load->database('db_lsima', TRUE);
+		echo json_encode(array('hasil'=>'1'));
 
-		$pin1 = $this->db->get_where('vc', array('pin'=>$pin));
-		$pin2 = $db_lsima->get_where('kunjungan', array('pin'=>$pin));
+		// $db_lsima = $this->load->database('db_lsima', TRUE);
 
-		if ($pin1->num_rows() == 1 && $pin2->num_rows() == 1) {
+		// $pin1 = $this->db->get_where('vc', array('pin'=>$pin));
+		// $pin2 = $db_lsima->get_where('kunjungan', array('pin'=>$pin));
 
-			$cek_status = $pin1->row()->status_selesai;
-			$channel = $pin1->row()->channel;
-			$cek_jam = $pin2->row()->tgl_kunjungan.' '.$pin2->row()->jam_vc.':00';
-			$selisih_menit = cek_time(get_waktu(),$cek_jam);
+		// if ($pin1->num_rows() == 1 && $pin2->num_rows() == 1) {
 
-			// log_r($selisih_menit);
-			if ($cek_status == '1' ) {
-				//waktu habis
-				// echo json_encode(array('hasil'=>'2'));
-				echo json_encode(array('hasil'=>'1','channel'=>$channel));
+		// 	$cek_status = $pin1->row()->status_selesai;
+		// 	$channel = $pin1->row()->channel;
+		// 	$cek_jam = $pin2->row()->tgl_kunjungan.' '.$pin2->row()->jam_vc.':00';
+		// 	$selisih_menit = cek_time(get_waktu(),$cek_jam);
 
-			} else {
-				if ($selisih_menit > 5) {
-					//waktu belum sampai
-					echo json_encode(array('hasil'=>'3','jadwal' => $cek_jam));
-				} elseif ($selisih_menit < -10){
-					//jadwal sudah lwat
-					// echo json_encode(array('hasil'=>'4'));
-					echo json_encode(array('hasil'=>'1','channel'=>$channel));
-				} else {
-					//waktu masih bisa
-					echo json_encode(array('hasil'=>'1','channel'=>$channel));
-				}
+		// 	// log_r($selisih_menit);
+		// 	if ($cek_status == '1' ) {
+		// 		//waktu habis
+		// 		// echo json_encode(array('hasil'=>'2'));
+		// 		echo json_encode(array('hasil'=>'1','channel'=>$channel));
+
+		// 	} else {
+		// 		if ($selisih_menit > 5) {
+		// 			//waktu belum sampai
+		// 			echo json_encode(array('hasil'=>'3','jadwal' => $cek_jam));
+		// 		} elseif ($selisih_menit < -10){
+		// 			//jadwal sudah lwat
+		// 			// echo json_encode(array('hasil'=>'4'));
+		// 			echo json_encode(array('hasil'=>'1','channel'=>$channel));
+		// 		} else {
+		// 			//waktu masih bisa
+		// 			echo json_encode(array('hasil'=>'1','channel'=>$channel));
+		// 		}
 				
-			}
+		// 	}
 
 			
-		} else {
-			echo json_encode(array('hasil'=>'0'));
-		}
+		// } else {
+		// 	echo json_encode(array('hasil'=>'0'));
+		// }
 	}
 
 	public function kembali($pin)
 	{
-		$this->db->where('channel', $pin);
-		$this->db->update('vc', array('channel'=>create_random(15)));
+		// $this->db->where('channel', $pin);
+		// $this->db->update('vc', array('channel'=>create_random(15)));
 		redirect('web','refresh');
 	}
 
